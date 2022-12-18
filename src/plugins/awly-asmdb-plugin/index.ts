c6e856d025a49332cfb156613be9d9834c104e89
WANG Xuerui <git@xen0n.name>
2022-12-18 14:08:29 +0800

    feat: implement awly-asmdb-plugin, build helpers in GHA builds

---
+++ src/plugins/awly-asmdb-plugin/index.ts
@@ -0,0 +1,60 @@
+import { spawn } from 'child_process'
+import path from 'path'
+
+import glob from 'glob-promise'
+
+import type { LoadContext, Plugin } from '@docusaurus/types'
+
+export interface PluginOptions {
+  genAsmdbDataPath: string
+  loongarchOpcodesPath: string
+}
+
+type LoadedContent = string
+
+function runGenerationScript(asmdbHelperPath: string, inputPaths: string[]): Promise<LoadedContent> {
+  return new Promise((resolve, reject) => {
+    const subp = spawn(asmdbHelperPath, inputPaths)
+    let stdout = ''
+    let stderr = ''
+    subp.stdout.on('data', (data) => stdout += data)
+    subp.stderr.on('data', (data) => stderr += data)
+    subp.on('close', (code) => {
+      if (code == 0) {
+        resolve(stdout)
+        return
+      }
+
+      console.error('gen-asmdb-data failed with', code)
+      console.error('stderr:', stderr)
+      reject('gen-asmdb-data failed')
+    })
+    subp.on('error', (err) => {
+      console.error('failed to spawn gen-asmdb-data:', err)
+      reject('failed to spawn gen-asmdb-data')
+    })
+  })
+}
+
+export default async function awlyAsmdbPlugin(
+  ctx: LoadContext,
+  options: PluginOptions,
+): Promise<Plugin<LoadedContent>> {
+  return {
+    name: 'awly-asmdb-plugin',
+    async loadContent() {
+      const opcodeFiles = await glob('*.txt', {cwd: options.loongarchOpcodesPath})
+      const inputPaths = opcodeFiles.map((x) => path.join(options.loongarchOpcodesPath, x))
+      return await runGenerationScript(options.genAsmdbDataPath, inputPaths)
+    },
+    async contentLoaded({content, actions}) {
+      await actions.createData('asmdb.json', content)
+    },
+    getPathsToWatch() {
+      return [
+        options.genAsmdbDataPath,
+        path.join(options.loongarchOpcodesPath, '*.txt'),
+      ]
+    },
+  }
+}
