e696b21ad454818e8d8ef17c695c240c696ff19b
WANG Xuerui <git@xen0n.name>
2023-04-01 19:50:06 +0800

    feat: implement bulk of ProjectPage

--- src/components/ProjectPage/index.tsx
+++ src/components/ProjectPage/index.tsx
@@ -2,14 +2,62 @@ import React from 'react'
 
 import Layout from '@theme/Layout'
 
-import { IProject } from '@site/src/types'
+import { IPortingEffort, IProject } from '@site/src/types'
+import SupportStatusIcon from '../SupportStatusIcon'
+import CodeQualityIcon from '../CodeQualityIcon'
+import styles from './styles.module.css'
+
+function PortingEffort({data}: {data: IPortingEffort}): JSX.Element {
+  return (
+    <section>
+      <dl className={styles.infoTable}>
+        {data.desc ?
+          <>
+            <dt>说明</dt>
+            <dd>{data.desc}</dd>
+          </>
+          : ''}
+        <dt>链接</dt>
+        <dd>{data.link ? <a href={data.link}>{data.link}</a> : '无'}</dd>
+        <dt>参与者</dt>
+        <dd>
+          <ul className={styles.authors}>
+            {data.authors.map((x, i) => (<li key={i}>{x}</li>))}
+          </ul>
+        </dd>
+        <dt>适配工作质量</dt>
+        <dd><CodeQualityIcon val={data.quality} showDesc={true} /></dd>
+        <dt>支持状态</dt>
+        <dd><SupportStatusIcon val={data.supportStatus} showDesc={true} /></dd>
+        <dt>何时开始支持</dt>
+        <dd>{data.releasedSinceVersion}</dd>
+        <dt>何时支持完善</dt>
+        <dd>{data.goodSinceVersion}</dd>
+      </dl>
+    </section>
+  )
+}
 
 export default function ProjectPage({data}: {data: IProject}): JSX.Element {
   return (
     <Layout title={`${data.name} | 项目详情`}>
-      <main>
+      <main className="container">
         <h2>{data.name}</h2>
-        TODO
+
+        <section>
+          <h3>项目概况</h3>
+          <dl className={styles.infoTable}>
+            <dt>首页</dt>
+            <dd>{data.homepageURL ? <a href={data.homepageURL}>{data.homepageURL}</a> : '暂缺'}</dd>
+            <dt>储存库</dt>
+            <dd>{data.repoURL ? <a href={data.repoURL}>{data.repoURL}</a> : '暂缺'}</dd>
+          </dl>
+        </section>
+
+        <section>
+          <h3>适配工作</h3>
+          {data.portingEfforts.map((x, i) => (<PortingEffort key={i} data={x} />))}
+        </section>
       </main>
     </Layout>
   );
--- src/components/SupportStatusIcon/index.tsx
+++ src/components/SupportStatusIcon/index.tsx
@@ -15,8 +15,31 @@ function emojiForSupportStatus(ss: SupportStatus): string {
   }
 }
 
-export default function SupportStatusIcon({val}: {val: SupportStatus}): JSX.Element {
+function descForSupportStatus(ss: SupportStatus): string {
+  switch (ss) {
+  case SupportStatus.Unknown: return '未知'
+  case SupportStatus.Rejected: return '被拒绝'
+  case SupportStatus.CommercialOnly: return '仅有付费商业支持'
+  case SupportStatus.Stalled: return '进度受阻'
+  case SupportStatus.UpForGrabs: return '目前无人认领，先到先得'
+  case SupportStatus.WIP: return '施工中'
+  case SupportStatus.UnderReview: return '正在接受代码审查'
+  case SupportStatus.WaitingRelease: return '万事俱备，就差上游发版'
+  case SupportStatus.Released: return '已在正式上游版本发布'
+  }
+}
+
+
+type Options = {
+  val: SupportStatus
+  showDesc?: boolean
+}
+
+export default function SupportStatusIcon({val, showDesc}: Options): JSX.Element {
   return (
-    <span>{emojiForSupportStatus(val)}</span>
+    <>
+      <span>{emojiForSupportStatus(val)}</span>
+      {showDesc ? <span>{descForSupportStatus(val)}</span> : ''}
+    </>
   );
 }
