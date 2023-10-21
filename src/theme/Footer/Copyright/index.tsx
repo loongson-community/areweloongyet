ec1d2c8dcf844f2da861a08725bb6b63cd03648a
WANG Xuerui <git@xen0n.name>
2023-10-22 01:29:39 +0800

    refactor: wrap the Footer/Copyright component
    `yarn swizzle @docusaurus/theme-classic Footer/Copyright --wrap` then
    translate the `index.js` into TSX

---
+++ src/theme/Footer/Copyright/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Copyright from '@theme-original/Footer/Copyright'
+
+export default function CopyrightWrapper(props: any): JSX.Element {
+  return (
+    <>
+      <Copyright {...props} />
+    </>
+  )
+}
