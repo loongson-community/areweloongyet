65a79204b7298c9bc83a5e73d0f47acc9f787734
WANG Xuerui <git@xen0n.name>
2023-10-27 18:22:37 +0800

    feat: enable the trailingSlash config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -30,6 +30,7 @@ const config = {
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+  trailingSlash: true, // to reduce number of 301's with GitHub Pages
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
