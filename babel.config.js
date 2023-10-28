b988998bdcb5179d50227406426433ed5934cf19
WANG Xuerui <git@xen0n.name>
2023-10-28 16:09:25 +0800

    build: silence warnings regarding some loose mode
    ```
    Though the "loose" option was set to "true" in your @babel/preset-env config, it will not be used for @babel/plugin-proposal-private-property-in-object since the "loose" mode option was set to "false" for @babel/plugin-proposal-class-properties.
    The "loose" option must be the same for @babel/plugin-proposal-class-properties, @babel/plugin-proposal-private-methods and @babel/plugin-proposal-private-property-in-object (when they are enabled): you can silence this warning by explicitly adding
            ["@babel/plugin-proposal-private-property-in-object", { "loose": false }]
    to the "plugins" section of your Babel config.
    ```

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
-  plugins: [['@babel/plugin-proposal-class-properties']],
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: false }],
+    ["@babel/plugin-proposal-private-methods", { loose: false }]
+  ],
   assumptions: {
     setPublicClassFields: false
   }
-};
+}
