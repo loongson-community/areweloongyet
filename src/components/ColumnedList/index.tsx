065cdac94ec0027e680d0b2f41be7b8ca7829daa
WANG Xuerui <git@xen0n.name>
2023-11-06 15:53:44 +0800

    feat: add a columned container component for adaptive layout
    otherwise the content will stay columned on narrow screens, which can
    get ugly

---
+++ src/components/ColumnedList/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react'
+
+import styles from './styles.module.css'
+
+type Props = {
+  columns: number
+  children?: ReactNode | undefined
+}
+
+export default function ColumnedList(props: Props): JSX.Element {
+  return (
+    <div className={styles.columnedList} style={{ columns: props.columns }}>
+      {props.children}
+    </div>
+  );
+}
