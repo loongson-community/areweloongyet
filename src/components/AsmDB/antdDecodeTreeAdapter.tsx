e377f4745370f0433346270b918e47f07f41f659
WANG Xuerui <git@xen0n.name>
2024-04-18 20:58:07 +0800

    refactor(asmdb): move well-known alias handling into decodetree augmentation

--- src/components/AsmDB/antdDecodeTreeAdapter.tsx
+++ src/components/AsmDB/antdDecodeTreeAdapter.tsx
@@ -16,40 +16,6 @@ type NodeTitleProps = {
   node?: AugmentedDecodeTreeNode
 }
 
-const wellKnownMatchPatterns = {
-  '000000xxxxxxxxxxxxxxxxxxxxxxxxxx': '运算',
-  '00000000000000000xxxxxxxxxxxxxxx': '双寄存器',
-  '00000000000100xxxxxxxxxxxxxxxxxx': '三寄存器-甲',
-  '00000000000101xxxxxxxxxxxxxxxxxx': '三寄存器-乙',
-  '00000000000110xxxxxxxxxxxxxxxxxx': '三寄存器-丙',
-  '00000000000111xxxxxxxxxxxxxxxxxx': '乘',
-  '00000000001000xxxxxxxxxxxxxxxxxx': '除',
-  '00000000001001xxxxxxxxxxxxxxxxxx': 'CRC',
-  '00000000010xxxxxxxxxxxxxxxxxxxxx': '移位',
-  '0000000100xxxxxxxxxxxxxxxxxxxxxx': 'FP 三寄存器',
-  '000001xxxxxxxxxxxxxxxxxxxxxxxxxx': '特权',
-  '00000110010010000xxxxxxxxxxxxxxx': 'IOCSR-TLB',
-  '000010xxxxxxxxxxxxxxxxxxxxxxxxxx': '四寄存器',
-  '000011xxxxxxxxxxxxxxxxxxxxxxxxxx': '比较选择',
-  '000011000001xxxxxxxxxxxxxxxxxxxx': '比较选择-FP FP32',
-  '000011000010xxxxxxxxxxxxxxxxxxxx': '比较选择-FP FP64',
-  '000011000101xxxxxxxxxxxxxxxxxxxx': '比较选择-LSX FP32',
-  '000011000110xxxxxxxxxxxxxxxxxxxx': '比较选择-LSX FP64',
-  '000011001001xxxxxxxxxxxxxxxxxxxx': '比较选择-LASX FP32',
-  '000011001010xxxxxxxxxxxxxxxxxxxx': '比较选择-LASX FP64',
-  '000101xxxxxxxxxxxxxxxxxxxxxxxxxx': '大立即数-甲',
-  '000110xxxxxxxxxxxxxxxxxxxxxxxxxx': '大立即数-乙',
-  '000111xxxxxxxxxxxxxxxxxxxxxxxxxx': '大立即数-丙',
-  '001000xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-LLSC',
-  '001001xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-大立即数',
-  '001010xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-整数与 FP',
-  '001011xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-SIMD 与 LBT',
-  '001100xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-SIMD 元素',
-  '001110xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-原子与复合',
-  '010010xxxxxxxxxxxxxxxxxxxxxxxxxx': '跳转-FP 与 LBT',
-  '011100xxxxxxxxxxxxxxxxxxxxxxxxxx': '运算-LSX',
-  '011101xxxxxxxxxxxxxxxxxxxxxxxxxx': '运算-LASX',
-}
 
 function NodeTitle({ match, node }: NodeTitleProps): JSX.Element {
   if (!match)
@@ -57,6 +23,7 @@ function NodeTitle({ match, node }: NodeTitleProps): JSX.Element {
 
   const matchNumber = match ? match.match : 0
   const matchPattern = node ? node.key : match.key
+  const alias = match?.wellKnownAlias || node?.wellKnownAlias
   const insn = match ? match.matched : ''
   const lookAt = node ? node.look_at : match.parentNode.look_at
   const parentLookAt = node ? node.parentLookAt : null
@@ -68,8 +35,8 @@ function NodeTitle({ match, node }: NodeTitleProps): JSX.Element {
       postAttribs.push(<span className={styles.contentAttrib} key={`${matchPattern}-fmt`}>并确定格式为 {match.fmt}</span>)
     else
       postAttribs.push(<span className={styles.attrib} key={`${matchPattern}-fmt`}>格式 {match.fmt}</span>)
-  if (wellKnownMatchPatterns.hasOwnProperty(matchPattern))
-    preAttribs.push(<span className={styles.attrib} key={`${matchPattern}-alias`}>{wellKnownMatchPatterns[matchPattern]}</span>)
+  if (alias)
+    preAttribs.push(<span className={styles.attrib} key={`${matchPattern}-alias`}>{alias}</span>)
 
   if (insn) {
     postAttribs.push(<span className={styles.attrib} key={`${matchPattern}-subspace`}>空间占用 {match.numUsedInsnWords}</span>)
--- src/components/AsmDB/augmentedDecodeTree.tsx
+++ src/components/AsmDB/augmentedDecodeTree.tsx
@@ -2,6 +2,47 @@ import _ from 'lodash'
 
 import { bitfieldWidth, mergeBitfields, restoreIntoBitfields } from './bitfield'
 
+const wellKnownMatchPatterns = {
+  '000000xxxxxxxxxxxxxxxxxxxxxxxxxx': '运算',
+  '00000000000000000xxxxxxxxxxxxxxx': '双寄存器',
+  '00000000000100xxxxxxxxxxxxxxxxxx': '三寄存器-甲',
+  '00000000000101xxxxxxxxxxxxxxxxxx': '三寄存器-乙',
+  '00000000000110xxxxxxxxxxxxxxxxxx': '三寄存器-丙',
+  '00000000000111xxxxxxxxxxxxxxxxxx': '乘',
+  '00000000001000xxxxxxxxxxxxxxxxxx': '除',
+  '00000000001001xxxxxxxxxxxxxxxxxx': 'CRC',
+  '00000000010xxxxxxxxxxxxxxxxxxxxx': '移位',
+  '0000000100xxxxxxxxxxxxxxxxxxxxxx': 'FP 三寄存器',
+  '000001xxxxxxxxxxxxxxxxxxxxxxxxxx': '特权',
+  '00000110010010000xxxxxxxxxxxxxxx': 'IOCSR-TLB',
+  '000010xxxxxxxxxxxxxxxxxxxxxxxxxx': '四寄存器',
+  '000011xxxxxxxxxxxxxxxxxxxxxxxxxx': '比较选择',
+  '000011000001xxxxxxxxxxxxxxxxxxxx': '比较选择-FP FP32',
+  '000011000010xxxxxxxxxxxxxxxxxxxx': '比较选择-FP FP64',
+  '000011000101xxxxxxxxxxxxxxxxxxxx': '比较选择-LSX FP32',
+  '000011000110xxxxxxxxxxxxxxxxxxxx': '比较选择-LSX FP64',
+  '000011001001xxxxxxxxxxxxxxxxxxxx': '比较选择-LASX FP32',
+  '000011001010xxxxxxxxxxxxxxxxxxxx': '比较选择-LASX FP64',
+  '000101xxxxxxxxxxxxxxxxxxxxxxxxxx': '大立即数-甲',
+  '000110xxxxxxxxxxxxxxxxxxxxxxxxxx': '大立即数-乙',
+  '000111xxxxxxxxxxxxxxxxxxxxxxxxxx': '大立即数-丙',
+  '001000xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-LLSC',
+  '001001xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-大立即数',
+  '001010xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-整数与 FP',
+  '001011xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-SIMD 与 LBT',
+  '001100xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-SIMD 元素',
+  '001110xxxxxxxxxxxxxxxxxxxxxxxxxx': '访存-原子与复合',
+  '010010xxxxxxxxxxxxxxxxxxxxxxxxxx': '跳转-FP 与 LBT',
+  '011100xxxxxxxxxxxxxxxxxxxxxxxxxx': '运算-LSX',
+  '011101xxxxxxxxxxxxxxxxxxxxxxxxxx': '运算-LASX',
+}
+
+function getWellKnownAlias(pat: string): string {
+  if (wellKnownMatchPatterns.hasOwnProperty(pat))
+    return wellKnownMatchPatterns[pat]
+  return ''
+}
+
 function makeMatchPatternKey(match: number, bfs: Bitfield[]): string {
   let s = match.toString(2).padStart(32, '0').split('')
   s.reverse()
@@ -15,6 +56,7 @@ function makeMatchPatternKey(match: number, bfs: Bitfield[]): string {
 
 export type AugmentedDecodeTreeMatch = DecodeTreeMatch & {
   key: string
+  wellKnownAlias: string
   rawMatch: number
   parentNode: AugmentedDecodeTreeNode
   next?: AugmentedDecodeTreeNode
@@ -23,6 +65,7 @@ export type AugmentedDecodeTreeMatch = DecodeTreeMatch & {
 
 export type AugmentedDecodeTreeNode = DecodeTreeNode & {
   key: string
+  wellKnownAlias: string
   rawMatch: number
   parentMatch?: AugmentedDecodeTreeMatch
   parentLookAt?: Bitfield[]
@@ -54,12 +97,14 @@ function augmentDecodeTreeInplace(
   node.numUsedInsnWords = 0
 
   node.key = makeMatchPatternKey(matchSoFar, matchBitfieldsSoFar)
+  node.wellKnownAlias = getWellKnownAlias(node.key)
   node.rawMatch = matchSoFar
   node.parentMatch = nodeMatch
   node.parentLookAt = nodeMatch ? nodeMatch.parentNode.look_at : []
   for (const m of node.matches) {
     const myMatch = matchSoFar | restoreIntoBitfields(m.match, node.look_at)
     m.key = makeMatchPatternKey(myMatch, myMatchBitfields)
+    m.wellKnownAlias = getWellKnownAlias(m.key)
     m.rawMatch = myMatch
     m.parentNode = node
 
