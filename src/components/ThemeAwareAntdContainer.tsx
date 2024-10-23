134e9d16537aa116f7b1054c20532d652cf9eb7d
WANG Xuerui <git@xen0n.name>
2024-10-23 18:41:40 +0800

    fix(asmdb): fix the /asmdb page's SSR behavior
    The page loads fine when one navigates to it after the page is fully
    loaded, but not when one accesses it directly (e.g. by opening it from a
    bookmark or a link shared via IM), signifying an SSR rehydration problem.
    
    Make the ThemeAwareAntdContainer component BrowserOnly to automatically
    fix all pages making use of antd, as we expect all antd usage to respect
    the Docusaurus color scheme.

--- src/components/ThemeAwareAntdContainer.tsx
+++ src/components/ThemeAwareAntdContainer.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { ConfigProvider, theme } from 'antd'
+import BrowserOnly from '@docusaurus/BrowserOnly'
 import { useColorMode } from '@docusaurus/theme-common'
 
 export default function ThemeAwareAntdContainer(props: React.PropsWithChildren): JSX.Element {
   const isDarkMode = useColorMode().colorMode == 'dark'
   const alg = isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm
-  return <ConfigProvider theme={{algorithm: alg}}>{props.children}</ConfigProvider>
+  return (
+    <BrowserOnly>
+      {() => <ConfigProvider theme={{algorithm: alg}}>{props.children}</ConfigProvider>}
+    </BrowserOnly>
+  )
 }
