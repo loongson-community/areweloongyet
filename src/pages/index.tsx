3b643eb92d228c8d28e1323e0d23b27625500c22
WANG Xuerui <git@xen0n.name>
2025-01-01 17:52:32 +0800

    feat(i18n): translate the rest of the homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,42 @@
 import clsx from 'clsx'
 
-import { translate } from '@docusaurus/Translate'
+import Translate, { translate } from '@docusaurus/Translate'
 import Layout from '@theme/Layout'
 import HomepageFeatures from '@site/src/components/HomepageFeatures'
 
 import styles from './index.module.css'
 
 function HomepageHeader() {
+  const slogan = translate({
+    id: 'awly.homepage.slogan',
+    message: '行则将至！',
+    description: 'Slogan of the homepage',
+  })
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">行则将至！</h1>
+        <h1 className="hero__title">{slogan}</h1>
         <p className="hero__subtitle">
-          Linux 开源生态的多数基础组件，其最新版本都已包含 LoongArch 支持了。
+          <Translate>
+            Linux 开源生态的多数基础组件，其最新版本都已包含 LoongArch 支持了。
+          </Translate>
           <br />
-          尽管龙芯公司仍未公开 LoongArch 手册的其余部分，但大量公开信息如
-          QEMU、内核适配已将相关指令编码与具体行为尽数披露，手册的缺失已不再能阻碍优化的脚步。
+          <Translate>
+            尽管龙芯公司仍未公开 LoongArch 手册的其余部分，但大量公开信息如
+            QEMU、内核适配已将相关指令编码与具体行为尽数披露，手册的缺失已不再能阻碍优化的脚步。
+          </Translate>
           <br />
-          我们预计将在 2023~2024 看到 LoongArch
-          新世界生态的突飞猛进，有您的参与会更快些。
+          <Translate>
+            我们预计将在 2023~2024 看到 LoongArch
+            新世界生态的突飞猛进，有您的参与会更快些。
+          </Translate>
           <br />
-          本站由社区建设维护，
+          <Translate>本站由社区建设维护，</Translate>
           <a href="https://github.com/loongson-community/areweloongyet">
-            欢迎来坐坐
+            <Translate>欢迎来坐坐</Translate>
           </a>
-          ！
+          <Translate>！</Translate>
         </p>
       </div>
     </header>
